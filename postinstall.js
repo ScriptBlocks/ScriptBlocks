96b9520e4669dcf08b0d84f6e8c9edd9bb258aae
OmgRod <rlimafonseca2010@gmail.com>
2024-10-29 18:12:52 +0000

    added windows signing

---
+++ postinstall.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config(); // Load .env variables
+
+const envFilePath = path.join(__dirname, '.env');
+const envVariables = fs.existsSync(envFilePath) ? fs.readFileSync(envFilePath, 'utf-8') : '';
+
+const envs = envVariables.split('\n').reduce((acc, line) => {
+  const [key, value] = line.split('=').map(v => v.trim());
+  if (key && value) {
+    acc[key] = value;
+  }
+  return acc;
+}, {});
+
+// Set the environment variables
+for (const key in envs) {
+  if (envs.hasOwnProperty(key)) {
+    process.env[key] = envs[key];
+  }
+}
+
+// Debugging output
+console.log('Loaded environment variables:');
+console.log('CSC_LINK:', process.env.CSC_LINK);
+console.log('CSC_KEY_PASSWORD:', process.env.CSC_KEY_PASSWORD);
